5c8a8c790a27f9988e7bc5e21c551a3a3b286c98
Vadim Lobanov <lobanov.vadim@undev.ru>
2014-11-14 13:14:14 +0300

    db config

--- server/src/frontier_config.erl
+++ server/src/frontier_config.erl
@@ -1,12 +1,19 @@
 -module(frontier_config).
 -export([server_port/0]).
 -export([server_tcp_pool_size/0]).
+-export([db_credentials/0]).
 
 server_port() ->
     {ok,[[Port_str]]} = init:get_argument(port),
     list_to_integer(Port_str).
 
 server_tcp_pool_size() ->
+    read_config_key(tcp_pool).
+
+db_credentials() ->
+    read_config_key(db_credentials).
+
+read_config_key(Key) ->
     {ok, [Info]} = file:consult("./frontier.config"),
-    {tcp_pool, Port} = lists:keyfind(tcp_pool, 1, Info),
-    Port.
+    {Key, Val} = lists:keyfind(Key, 1, Info),
+    Val.
