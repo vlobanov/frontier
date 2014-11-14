8b3edcfdcfd3407e295bea920b12d8a471de3ba4
Vadim Lobanov <lobanov.vadim@undev.ru>
2014-11-14 13:13:57 +0300

    fix typo in start script

--- server/start.sh
+++ server/start.sh
@@ -6,7 +6,7 @@ then
 	mkdir -p ./logs/$1
 	rm -rf /tmp/frontier_$1
 	mkdir -p /tmp/frontier_$1
-	erl -port $ALLARGS -sname admy_ad_$1@localhost -mnesia dir "'/tmp/frontier_$1'" -pa ebin deps/*/ebin -s frontier \
+	erl -port $ALLARGS -sname frontier_$1@localhost -mnesia dir "'/tmp/frontier_$1'" -pa ebin deps/*/ebin -s frontier \
 		-eval "io:format(\"frontier caching started on port $1~n\")."
 else  
 	echo "You should specify port\n"
