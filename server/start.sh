e2e871331a48deb1d6835947e238e762d573d89e
Vadim Lobanov <lobanov.vadim@undev.ru>
2014-11-15 18:35:06 +0300

    more nofile limit, other erl flags

--- server/start.sh
+++ server/start.sh
@@ -2,11 +2,12 @@
 if [ $1 ]
 then
 	ALLARGS=$*
-	ulimit -n 10000
+	ulimit -n 60000
 	mkdir -p ./logs/$1
 	rm -rf /tmp/frontier_$1
 	mkdir -p /tmp/frontier_$1
-	erl -port $ALLARGS -sname frontier_$1@localhost -mnesia dir "'/tmp/frontier_$1'" -pa ebin deps/*/ebin -s frontier \
+    ulimit -a
+	erl +Q 64000 +P 128000 -port $ALLARGS -sname frontier_$1@localhost -mnesia dir "'/tmp/frontier_$1'" -pa ebin deps/*/ebin -s frontier \
 		-eval "io:format(\"frontier caching started on port $1~n\")."
 else  
 	echo "You should specify port\n"
